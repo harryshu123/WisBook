f1abb708f0506a0375fd184e304de7170837283c
withteja <csaiteja533@gmail.com>
2019-12-02 20:03:36 -0600

    buttons

--- Local/application/Main.java
+++ Local/application/Main.java
@@ -1,10 +1,24 @@
+/*
+ Team : A40
+ Project a2(Initial GUI page) Social Network
+ Team Members : Chokkarapu Sai Teja , Lintong Han, Zhihao Shu , Lakshay
+ Date : 12/2/19
+*/
+
+
+
 package application;
 
 
 import java.io.FileInputStream;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 import java.util.Random;
+import java.util.Set;
+
 import javafx.scene.shape.Line;
 import javafx.application.Application;
 import javafx.collections.FXCollections;
@@ -43,39 +57,26 @@ import javafx.scene.text.TextBoundsType;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-	
+	class vector{
+	int x,y;
+		
+		vector(int a, int b){
+			x=a;
+			y=b;
+		}
+	};
 
-
+int fl=5;
 	// store any command-line arguments that were entered.
 	// NOTE: this.getParameters().getRaw() will get these also
 	private List<String> args;
 
-	private static final int WINDOW_WIDTH = 3000;
-	private static final int WINDOW_HEIGHT = 600;
+	private static final int WINDOW_WIDTH = 1000;
+	private static final int WINDOW_HEIGHT = 1000;
 	private static final String APP_TITLE = "Hello to WisBook!";
-	Random random = new Random();
-    int numberOfRows = 25;
-    int numberOfColumns = 25;
-    public void addCirclesToGridPane(GridPane gridPane, List<Circle> circles)
-    {
-        
-            gridPane.add(circles.get(0), 0, 0);
-        
-    }
-    private static final int R = 10;
-    private static final Color lineColor = Color.FIREBRICK.deriveColor(0, 1, 1, .6);
-    private Circle createCircle() {
-        final Circle circle = new Circle(R);
-
-        circle.setStroke(Color.WHITE);
-        circle.setStrokeWidth(10);
-        circle.setStrokeType(StrokeType.INSIDE);
-        circle.setFill(Color.WHITE);
-        circle.relocate(0, 0);
-
-        return circle;
-    }
-
+	
+    
+int c =15;
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		
@@ -85,78 +86,80 @@ public class Main extends Application {
 		args = this.getParameters().getRaw();
 
 		//set top label
-		Label title = new Label("WisBook");
-	
-		
-      
-		
-		
-		
-		
-		/////
-//		"0"(0,2)
-//				1(1,0).(drawlineto(0,2))		
+		Label title = new Label("WisBook");	
            
         	GridPane pane = new GridPane();
-        	Circle circle = createCircle();
-    		Text text = new Text("42");
-    		text.setBoundsType(TextBoundsType.VISUAL); 
-    	
-//    		pane.getChildren().addAll(circle, text);
-         
-		
-		//pane.add(name1,0,0);
+   
+
+		// Creates a canvas that can draw shapes and text
+				Canvas canvas = new Canvas(WINDOW_WIDTH*(0.7), WINDOW_HEIGHT*(0.7));
+				GraphicsContext gc = canvas.getGraphicsContext2D();
+				// Write some text
+				// Text is filled with the fill color
+				gc.setFill(Color.BLUE);
+			HashMap<Integer,vector> map = new HashMap<Integer,vector>();
+//			HashMap<Integer,vector> map1 = new HashMap<Integer,vector>();
+//			
+		map.put(0,new vector(150,150));
+		map.put(1,new vector(550,550));
+		map.put(2,new vector(550,150));
+		map.put(3,new vector(150,550));
+		map.put(4,new vector(350,150));
+		map.put(5,new vector(350,550));
+		map.put(6,new vector(150,350));
+		map.put(7,new vector(550,350));
+ int j =1;
+ int temp =fl;
+	for(Map.Entry<Integer,vector> i : map.entrySet()) {
+		if(temp==0)break;
+		String k="Friend "+j++;
+		gc.fillText(k,	i.getValue().x,i.getValue().y-10);
+		gc.fillOval(i.getValue().x,i.getValue().y, 30, 30);temp--;
 	
 		
-		//nn sm sjis n s msmdns dsi  sins f sR nfksn/sjn s  
-		// sdnjv snd ksd sn m sj f n sf
-		// the main fucrion the main file in the msantsh s=gat  rnubs  jsn  the main insert file 
-		VBox box = new VBox();
-		Line line = new Line();
-		Button name1= new Button("name1");
-		pane.add(name1,0, 0);
-		double x= name1.getLayoutX();
-		double y = name1.getLayoutY();
-		System.out.println(x+" "+y);
-		
-		Button  Center = new Button ("Center People");
-		
-		pane.add(Center,1,1);
-		
-		Button name3= new Button("name3");
-		
-		pane.add(name3,0,2);
-		name3.setLayoutX(0);
-		name3.setLayoutY(2);
-		double x1= name3.getLayoutX();
-		double y1 = name3.getLayoutY();
-		System.out.println(x1+" "+y1);
-		pane.add(new Button("name4"),2,0);
-		pane.add(new Button("name5"),2,2);
-		
-		line.setStartX(100);
-        line.setStartY(0);
-        line.setEndX(100);
-        line.setEndY(-100);
-        pane.getChildren().add(line);
-		//right box
+		gc.setStroke(Color.BLUE);
+				
+				gc.setLineWidth(2);
+				gc.strokeLine(350+c, 350+c, i.getValue().x+c, i.getValue().y+c);
+	}
+				
+				
+				// Draw a few circles
+				gc.setFill(Color.BLACK);
+				
+				// center
+				gc.fillText("Center Person",350,350-10);
+				gc.setFill(Color.RED);
+				gc.fillOval(350,350, 30, 30);
+				
+				
+				pane.getChildren().add(canvas);
+
 		VBox info = new VBox();
-		info.getChildren().add(new Label("Center People Info"));
+		info.getChildren().add(new Label("Friends List"));
 		
 		//bottom button
+		System.out.println(c);
+	ListView<Button> list = new ListView<Button>();
+		for(int i =1;i<=fl;i++) { 
+				
+			String k="Friend"+i;
+			Button b;
+		b = new Button (k);
+		list.getItems().add(b);
+//		ObservableList<Button> items = FXCollections.observableArrayList (b);
+//		
+//		list.setItems(items);
+		}
+		info.getChildren().add(list);
 	
 		
 		
-		ListView<String> list = new ListView<String>();
-		ObservableList<String> items = FXCollections.observableArrayList (
-			    "friend1", "friend2", "friend3", "friend4");
-		list.setItems(items);
-		info.getChildren().add(list);
 		BorderPane root = new BorderPane();
 		
 		
 		
-		TextField text1 = new TextField("Please enter the person: ");
+		TextField text1 = new TextField("Enter and click Submit");
 		
 		ListView<String> allpeople = new ListView<String>();
 		ObservableList<String> people = FXCollections.observableArrayList (
@@ -167,7 +170,7 @@ public class Main extends Application {
 		//log page
 		BorderPane log = new BorderPane();
 
-		Scene scene = new Scene(log,WINDOW_WIDTH, WINDOW_HEIGHT);
+		Scene scene = new Scene(log,WINDOW_WIDTH, WINDOW_HEIGHT*(0.7));
 		
 		Scene mainScene = new Scene(root, WINDOW_WIDTH, WINDOW_HEIGHT);
 		Button submit = new Button("submit");
@@ -182,7 +185,7 @@ public class Main extends Application {
         BorderPane.setAlignment(submit, Pos.CENTER);
 		 submit.setOnAction(event1);
 		//button to the next page
-		
+		Circle c = new Circle();
 		//set left 
 		log.setLeft(text1);
 		BorderPane.setAlignment(text1, Pos.CENTER);
@@ -193,27 +196,25 @@ public class Main extends Application {
 		log.setBottom(submit);
 		BorderPane.setAlignment(submit, Pos.CENTER);
 
-		// Add the vertical box to the center of the root pane
 		root.setCenter(info);
 		root.setTop(title);
 		root.setLeft(pane);
-		
-		
+
 		BorderPane.setAlignment(info, Pos.CENTER);
 		
 		BorderPane.setAlignment(title, Pos.CENTER);
 		BorderPane.setAlignment(pane, Pos.BOTTOM_CENTER);
 
 		
-		   Button b = new Button ("Back");
+		   Button b1 = new Button ("Back");
 	        EventHandler<ActionEvent> event = new EventHandler<ActionEvent>() {
 	            public void handle(ActionEvent e)
 	            {
 	                primaryStage.setScene(scene);           }
 	        };
-	        root.setBottom(b);
-	        BorderPane.setAlignment(b, Pos.CENTER);
-			 b.setOnAction(event);
+	        root.setBottom(b1);
+	        BorderPane.setAlignment(b1, Pos.CENTER);
+			 b1.setOnAction(event);
 			
 		BorderPane.setAlignment(submit, Pos.CENTER);
 		// Add the stuff and set the primary stage
